25857187444fec2c545053131ef7541ac1b6415b
pkpndLiZ <phanida.lip@gmail.com>
2023-04-18 11:17:56 +0700

    create Layout, Navbar, Owner components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./Home";
+import Owner from "./Owner";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={} />
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/owner",
+    element: <Owner />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
